10479c5221b358e465e1f0d4f41d78654abb9d69
onkar lonkar <onkarlonkar55@gmail.com>
2026-01-14 23:17:54 +0530

    Checking_New_Solutions_Temprature_range_classification

---
+++ temperature_range_classification/Program.cs
@@ -0,0 +1,23 @@
+﻿// Take a temperature value and print “Cold”, “Warm”, or “Hot” using range conditions.
+namespace temperature_range_classification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Temprateure ");
+            int Temp = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Temp < 10)
+                Console.WriteLine("Cold");
+            else if (Temp > 10 && Temp < 20)
+            {
+                Console.WriteLine("Warm");
+            }
+            else
+                Console.WriteLine("Hot");
+         
+        }
+    }
+}
