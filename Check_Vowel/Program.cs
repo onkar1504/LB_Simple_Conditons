6afa48fcc36766ee96a250fde0d50c0759de5741
onkar lonkar <onkarlonkar55@gmail.com>
2026-01-15 22:18:17 +0530

    Checking _ TypeIdentificaation & Check Vowel Code

---
+++ Check_Vowel/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Check_Vowel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a character: ");
+            char ch = Console.ReadLine()[0];
+
+            if("aeiouAEIOU".Contains(ch))
+            {
+                Console.WriteLine("Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Constent");
+            }
+        }
+    }
+}
